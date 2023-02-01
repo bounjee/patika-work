8ab6de0e6d6506fc0829506a591e2e4523204684
bounjee <69115100+bounjee@users.noreply.github.com>
2023-02-01 23:02:40 +0300

    Add files via upload

---
+++ patika-w1/js/clock.js
@@ -0,0 +1,45 @@
+let username = prompt("What's your name? ")
+let myName = document.querySelector("#myName")
+myName.innerHTML = `${username} `
+
+// let myImg = document.querySelector("#resimg")   
+// myImg.src 
+
+const myImg = document.querySelector("#imgs")
+myImg.src ="res.jpg";
+myImg.style.margin = "0 auto";
+document.body.appendChild(myImg);
+
+
+
+
+
+// let clock = document.querySelector("#myClock")
+// let currentTime = new Date().toLocaleTimeString()
+// clock.innerHTML = `${currentTime}`
+
+function currentTimeModule() {
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session  = "AM"
+    var gunler = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];  
+
+    if (hh > 12 ) {
+        session = "PM";
+    }
+
+    hh = (hh < 10 ) ? "0" + hh : hh;
+    mm = ( mm < 10) ? "0" + mm : mm;
+    ss = ( ss < 10 ) ? "0" + ss : ss;
+
+    let time = hh + ":" + mm + ":" + ss + " " + session + " " + gunler[date.getDay()];
+
+    document.getElementById("myClock").innerText = time; 
+    var t = setTimeout(function() {currentTimeModule()},1000);
+
+}
+
+
+currentTimeModule();
